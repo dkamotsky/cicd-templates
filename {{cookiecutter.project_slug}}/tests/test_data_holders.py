848147d76955a707b98b6ffd57360bb6c75ac3a1
Denis Kamotsky <dkamotsky@gmail.com>
2020-08-19 19:14:05 -0600

    Imports as part of test.

--- {{cookiecutter.project_slug}}/tests/test_data_holders.py
+++ {{cookiecutter.project_slug}}/tests/test_data_holders.py
@@ -1,9 +1,7 @@
 import pytest
 import numpy as np
-import tensorflow as tf
 from oktest import ok
 from itertools import cycle
-from {{cookiecutter.project_slug}}.data import WrappedTensorflowDataHolder
 
 
 DS_LEN: int = 131
@@ -14,6 +12,7 @@ DS_WRITE_BATCH: int = 7
 
 @pytest.fixture
 def dataset():
+    import tensorflow as tf
     iterator = cycle(range(DS_LEN))
     def _gen():
         yield from iterator
@@ -21,6 +20,8 @@ def dataset():
 
 
 def test_wrapper(dataset):
+    import tensorflow as tf
+    from {{cookiecutter.project_slug}}.data import WrappedTensorflowDataHolder
     wrapper = WrappedTensorflowDataHolder(dataset, DS_LEN, DS_LIMIT, DS_WRITE_BATCH, False)
     wrapper.oneshot = True
     with wrapper as (ds, one_epoch_in_batches):
