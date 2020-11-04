c1ae3d6e07e2b97c8705cb84876e614bb240fa19
Denis Kamotsky <dkamotsky@gmail.com>
2020-11-04 13:21:03 -0700

    Adding overrides.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/jobs/sample/entrypoint.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/jobs/sample/entrypoint.py
@@ -1,8 +1,10 @@
 from {{cookiecutter.project_slug}}.jobs import Job
+from overrides import overrides
 
 
 class SampleJob(Job):
 
+    @overrides
     def init_adapter(self):
         if not self.conf:
             self.logger.info("Init configuration was not provided, using configuration from default_init method")
@@ -13,14 +15,16 @@ class SampleJob(Job):
         else:
             self.logger.info("Init configuration is already provided")
 
+    @overrides
     def launch(self):
-        self.logger.info("Launching bootstrap job")
+        self.logger.info("Launching sample job")
 
         df = self.spark.range(0, 1000)
 
         df.write.format(self.conf["output_format"]).mode("overwrite").save(self.conf["output_path"])
 
-        self.logger.info("Bootstrap job finished!")
+        self.logger.info("Sample job finished!")
+
 
 if __name__ == "__main__":
     job = SampleJob()
