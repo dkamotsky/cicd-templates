0b40e705c70f1f856d93922c212d20666ce0b5d0
Denis Kamotsky <dkamotsky@gmail.com>
2020-11-04 17:24:19 -0700

    Fixing bugs

--- {{cookiecutter.project_slug}}/notebooks/Use Deployed Library.py
+++ {{cookiecutter.project_slug}}/notebooks/Use Deployed Library.py
@@ -2,20 +2,20 @@
 import re
 from mlflow.tracking import MlflowClient
 mlflow_client = MlflowClient()
-ci_holder_name = "cet_debris_detection_cicd"
-versions = mlflow_client.get_latest_versions(ci_holder_name, stages=["Production"])
-assert len(versions) == 1
-ci_holder = versions[0]
-source_run = mlflow_client.get_run(ci_holder.run_id)
-dist_info = [fi for fi in mlflow_client.list_artifacts(source_run.info.run_id, 'dist') if fi.path.endswith('.whl')]
+experiment = mlflow_client.get_experiment_by_name('{{cookiecutter.workspace_dir}}')
+run_infos = mlflow_client.list_run_infos(experiment.experiment_id)
+run_infos = [ri for ri in run_infos if ri.status=='FINISHED']
+assert len(run_infos) > 0
+source_run_info = run_infos[0]
+dist_info = [fi for fi in mlflow_client.list_artifacts(source_run_info.run_id, 'dist') if fi.path.endswith('.whl')]
 assert len(dist_info) == 1
 dist_info = dist_info[0]
-lib_path = f"{source_run.info.artifact_uri}/{dist_info.path}"
+lib_path = f"{source_run_info.artifact_uri}/{dist_info.path}"
 lib_path = re.sub(r"^dbfs:/", "/dbfs/", lib_path)
-job_info = [fi for fi in mlflow_client.list_artifacts(source_run.info.run_id, 'job') if fi.path.endswith('runtime_requirements.txt')]
+job_info = [fi for fi in mlflow_client.list_artifacts(source_run_info.run_id, 'conf') if fi.path.endswith('cloud_requirements.txt')]
 assert len(job_info) == 1
 job_info = job_info[0]
-req_path = f"{source_run.info.artifact_uri}/{job_info.path}"
+req_path = f"{source_run_info.artifact_uri}/{job_info.path}"
 req_path = re.sub(r"^dbfs:/", "/dbfs/", req_path)
 print(lib_path)
 print(req_path)
