402118894c053337ee01d88753392b4fb32ccb39
Denis Kamotsky <dkamotsky@gmail.com>
2020-11-04 19:40:27 -0700

    Updated Docker configs.

--- {{cookiecutter.project_slug}}/docker/scripts/run_pipeline.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-python -u /{{cookiecutter.project_slug}}/run_pipeline pipelines --pipeline-name $@
-
--- {{cookiecutter.project_slug}}/tests/main.sh
+++ {{cookiecutter.project_slug}}/tests/main.sh
@@ -4,10 +4,7 @@ echo "Running static Python type checks..."
 echo
 echo
 mypy --config-file ./tests/mypy.ini {{cookiecutter.project_slug}} && \
-mypy --config-file ./tests/mypy.ini tests && \
-for dir in `find dev-tests -maxdepth 1 -mindepth 1 -type d`; do mypy --config-file ./tests/mypy.ini $dir; done && \
-for dir in `find integration-tests -maxdepth 1 -mindepth 1 -type d`; do mypy --config-file ./tests/mypy.ini $dir; done && \
-for dir in `find pipelines -maxdepth 1 -mindepth 1 -type d`; do mypy --config-file ./tests/mypy.ini $dir; done
+mypy --config-file ./tests/mypy.ini tests
 
 if [ $? -eq 0 ];then
     echo
@@ -23,7 +20,7 @@ fi
 echo "Running static PyLint ERROR-ONLY checks..."
 echo
 echo
-pylint --rcfile=./tests/pylint.rc -E {{cookiecutter.project_slug}} tests dev-tests integration-tests pipelines
+pylint --rcfile=./tests/pylint.rc -E {{cookiecutter.project_slug}} tests
 
 if [ $? -eq 0 ];then
     echo
@@ -42,7 +39,7 @@ echo
 cd tests
 export PYTHONPATH="$PYTHONPATH:.."
 echo "Modified for unit tests: PYTHONPATH = $PYTHONPATH"
-python -m pytest
+python -m pytest unit
 
 if [ $? -eq 0 -o $? -eq 5 ];then
     echo
