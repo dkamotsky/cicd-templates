301709a499499c610212b600efa5698075b6a77d
Denis Kamotsky <dkamotsky@gmail.com>
2020-11-13 12:00:19 -0700

    Fixed bugs.

--- {{cookiecutter.project_slug}}/tests/unit/generation_step_test.py
+++ {{cookiecutter.project_slug}}/tests/unit/generation_step_test.py
@@ -9,8 +9,13 @@ from pyspark.sql import SparkSession
 class GenerationStepUnitTest(unittest.TestCase):
 
     def setUp(self):
-        self.env: UnitTestEnvironment = UnitTestEnvironment(spark=SparkSession.builder.master("local[1]").getOrCreate(),
-                                                            conf="generation_step_test.yaml")
+        spark: SparkSession = SparkSession.builder.master("local[1]").\
+            config("spark.jars.packages", "io.delta:delta-core_2.12:0.7.0").\
+            config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension").\
+            config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog").\
+            config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore").\
+            getOrCreate()
+        self.env: UnitTestEnvironment = UnitTestEnvironment(spark=spark, conf="generation_step_test.yaml")
         self.step: GenerationStep = GenerationStep(self.env.spark, **self.env.conf)
         self.step.output_path = tempfile.TemporaryDirectory().name
 
